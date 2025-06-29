d9e27f18083de84049c05ff903cd1638cde4cafb
Sanjay-adepu <adepusanjay812@gmail.com>
2025-06-29 22:49:41 +0530

    Create Navbar.js

---
+++ src/app/Navbar.js
@@ -0,0 +1,58 @@
+"use client";
+
+import { useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import styles from "./Navbar.module.css"; // Assuming you're using CSS Modules
+
+const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles["navbar-container"]}>
+        {/* Logo */}
+        <Link href="/" className={styles["navbar-logo"]}>
+          <div className={styles["logo-wrapper"]}>
+            <Image
+              src="/file_00000000c9b4622f90d4953aa2f0956e.jpg"
+              alt="Droplin Logo"
+              width={40}
+              height={40}
+              className={styles["logo-img"]}
+            />
+            <span>Droplin</span>
+          </div>
+        </Link>
+
+        {/* Desktop Nav Links */}
+        <ul className={styles["nav-links-desktop"]}>
+          <li><Link href="/">Home</Link></li>
+          <li><Link href="/Upload">Upload</Link></li>
+          <li><Link href="/download">Download</Link></li>
+        </ul>
+
+        {/* Mobile Hamburger */}
+        <button
+          className={`${styles["hamburger-menu"]} ${isMenuOpen ? styles.open : ""}`}
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+        >
+          <div className={styles.bar}></div>
+          <div className={styles.bar}></div>
+          <div className={styles.bar}></div>
+        </button>
+      </div>
+
+      {/* Sidebar */}
+      <div className={`${styles.sidebar} ${isMenuOpen ? styles.open : ""}`}>
+        <ul className={styles["nav-links"]}>
+          <li><Link href="/" onClick={() => setIsMenuOpen(false)}>Home</Link></li>
+          <li><Link href="/Upload" onClick={() => setIsMenuOpen(false)}>Upload</Link></li>
+          <li><Link href="/download" onClick={() => setIsMenuOpen(false)}>Download</Link></li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
