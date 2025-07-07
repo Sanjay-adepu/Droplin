1f960d68d6fac8f88d2d6d8b872466eedf5a08aa
Sanjay-adepu <adepusanjay812@gmail.com>
2025-07-07 18:46:56 +0530

    Update page.js

--- src/app/upload/page.js
+++ src/app/upload/page.js
@@ -82,7 +82,9 @@ const UploadInterface = () => {
 <Navbar/>
     <div className={styles.uploadContainer}>
       <div className={styles.instructions}>
-       
+       <h3 style={{ fontSize: '20px', fontWeight: '600', color: '#111827', marginBottom: '1rem' }}>
+  How to upload
+</h3>
         <p>
           1. Select the type of data you want to upload: <strong>Files</strong>, <strong>Text</strong>, or <strong>Link</strong>.<br />
           2. Based on your selection, provide the required input and click <strong>Submit</strong>.
